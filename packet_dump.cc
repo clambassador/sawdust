6ef3dccdf4e93c74f9402f4afef054005e02af0a
Joel Reardon <joel@clambassador.com>
2018-03-30 19:19:35 -0700

    Added packetdump

---
+++ packet_dump.cc
@@ -0,0 +1,46 @@
+#include <cassert>
+#include <fstream>
+#include <string>
+
+#include "i_processor.h"
+#include "packet.h"
+#include "ib/formatting.h"
+#include "ib/tokenizer.h"
+
+#include <leveldb/db.h>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+        Config::_()->load("sawdust.cfg");
+
+	leveldb::DB* db;
+	if (argc < 2) {
+		Logger::info("usage: ./packet hash [highlight pos]");
+		return -1;
+	}
+
+	leveldb::Options options;
+	options.create_if_missing = false;
+	leveldb::Status status = leveldb::DB::Open(
+		options, Config::_()->gets("packetdb"), &db);
+	assert(status.ok());
+
+	string data;
+	db->Get(leveldb::ReadOptions(), argv[1], &data);
+	delete db;
+	if (data.empty()) {
+		Logger::info("no packet found for %", argv[1]);
+		return -1;
+	}
+	if (argc == 2) {
+		cout << data << endl;
+	} else {
+		int pos = atoi(argv[2]);
+		assert(pos >= 0 && pos < data.length());
+		cout << data.substr(0, pos);
+		cout << "\033[1;31m" << data[pos] << "\033[0m" <<
+		    data.substr(pos + 1);
+	}
+	return 0;
+}
